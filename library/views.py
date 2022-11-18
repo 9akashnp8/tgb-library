7315901e4daa38a722f8afcc0d69353bc4bd9288
Akash NP <akashnp1998@gmail.com>
2022-11-18 21:14:25 +0530

    Add Generic ListView for Author List

--- library/views.py
+++ library/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import ListView, TemplateView
+
+from .models import Author
 
 # Create your views here.
+class Home(TemplateView):
+    template_name = 'home.html'
+
+class AuthorListView(ListView):
+    model = Author
+    template_name = 'author/author_list.html'
