cf19a05cf5d8b79f1ea1663f19c62c29472126b2
Akash NP <91617769+9akashnp8@users.noreply.github.com>
2022-11-19 05:49:30 +0530

    Add Filtering for Author List: SPA like/No Refresh (#1)
    * Add django-filter app
    
    * Setup basic Model Filter with exact match as lookup_expr
    
    * Update View & Template to use Filtered Queryset, default=objects.all()
    
    * Add SPA like Filtering using HTMX
    
    No Refresh Filtered results using HTMX and partial htmls brings in better UX

---
+++ library/filters.py
@@ -0,0 +1,7 @@
+import django_filters
+from .models import Author
+
+class AuthorFilter(django_filters.FilterSet):
+    class Meta:
+        model = Author
+        fields = ['age', 'gender', 'name']
--- library/urls.py
+++ library/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import Home, author_list_and_create_view
+from .views import Home, author_list_and_create_view, authors_list
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
     path('author/', author_list_and_create_view, name='author_list'),
+
+    # HTMX
+    path('htmx/author/filter', authors_list, name='author-filter'),
 ]
