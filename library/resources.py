d836c08369d300a4f2dfab4bb737238dafddca78
Akash NP <91617769+9akashnp8@users.noreply.github.com>
2022-11-19 17:52:47 +0530

    Add Book Related Functionality (#3)
    * Add Search by Author Name functionality with icontains lookup_expr
    
    * Add Book Functionality (List, Create & SPA Filter)
    
    * Add Export Functionality for Book

--- library/resources.py
+++ library/resources.py
@@ -1,6 +1,10 @@
 from import_export import resources
-from .models import Author
+from .models import Author, Book
 
 class AuthorResource(resources.ModelResource):
     class Meta:
         model = Author
+
+class BookResource(resources.ModelResource):
+    class Meta:
+        model = Book
--- library/views.py
+++ library/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.http import HttpResponse
 
-from .models import Author
-from .forms import AuthorCreateForm
-from .filters import AuthorFilter
-from .resources import AuthorResource
+from .models import Author, Book
+from .forms import AuthorCreateForm, BookCreateForm
+from .filters import AuthorFilter, BookFilter
+from .resources import AuthorResource, BookResource
 
 # Create your views here.
 class Home(TemplateView):
@@ -28,6 +28,18 @@ def author_list_and_create_view(request):
     context = {'form': form, 'filter': filter}
     return render(request, 'author/author_list.html', context)
 
+def book_list_and_create_view(request):
+    filter = BookFilter(request.GET, queryset=None)
+    form = BookCreateForm()
+    if request.method == "POST":
+        form = BookCreateForm(request.POST)
+        if form.is_valid():
+            instance = form.save()
+            messages.success(request, f'{instance.name} Successfuly Added as a Book.')
+            return redirect('/book/')
+    context = {'form': form, 'filter': filter}
+    return render(request, 'author/book_list.html', context)
+
 # Helpers
 def export_author(request):
     author_resource = AuthorResource()
@@ -36,8 +48,20 @@ def export_author(request):
     response['Content-Disposition'] = 'attachment;filename="authors.csv"'
     return response
 
+def export_book(request):
+    author_resource = BookResource()
+    dataset = author_resource.export()
+    response = HttpResponse(dataset.csv, content_type='text/csv')
+    response['Content-Disposition'] = 'attachment;filename="books.csv"'
+    return response
+
 # HTMX Helpers
 def authors_list(request):
     filter = AuthorFilter(request.GET, queryset=Author.objects.all())
+    context = { 'filter': filter}
+    return render(request, 'partials/author_filtered_result.html', context)
+
+def books_list(request):
+    filter = BookFilter(request.GET, queryset=Book.objects.all())
     context = { 'filter': filter}
     return render(request, 'partials/author_filtered_result.html', context)
