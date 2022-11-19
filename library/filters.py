591577d690413839701e7d11ad9ac974312bb068
Akash NP <91617769+9akashnp8@users.noreply.github.com>
2022-11-19 06:38:52 +0530

    Add Export Functionality for Authors (#2)
    * Add Search by Author Name functionality with icontains lookup_expr
    
    * Add Export Functionality for Authors
    
    - This allows user to easily export all Author info to csv

--- library/filters.py
+++ library/filters.py
@@ -2,6 +2,7 @@ import django_filters
 from .models import Author
 
 class AuthorFilter(django_filters.FilterSet):
+    name = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
     class Meta:
         model = Author
         fields = ['age', 'gender', 'name']
